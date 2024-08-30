105423d17e46be50f6076cbe0548ea555404a2b5
mcalmius <markus.calmius@proton.ch>
2024-08-30 08:46:11 +0200

    re-did preserved accounts

--- src/main/java/com/inalogy/midpoint/connectors/freeipa/FreeIpaConnector.java
+++ src/main/java/com/inalogy/midpoint/connectors/freeipa/FreeIpaConnector.java
@@ -17,11 +17,7 @@ package com.inalogy.midpoint.connectors.freeipa;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.net.URISyntaxException;
 
 import org.apache.http.NameValuePair;
@@ -139,6 +135,9 @@ public class FreeIpaConnector extends AbstractRestConnector<FreeIpaConfiguration
 	
 	// Free IPA schema cache
 	private static JSONObject schema = null;
+
+	// this should probably be done in a better way
+	private static Set<String> preserved_user_list = new HashSet<>();
 	
     @Override
     public void init(Configuration configuration) {
@@ -590,6 +589,13 @@ public class FreeIpaConnector extends AbstractRestConnector<FreeIpaConfiguration
             String key = keys.next();
         	Object value = user.get(key); 
 //    		LOG.ok("JSON key:{0}={1}", key, value);
+			if(getConfiguration().getSupportPreserved()){
+				if (key.contains("preserved")) {
+					if (Boolean.TRUE.equals(value)){
+						preserved_user_list.add(uid);
+					}
+				}
+			}
             if (value instanceof JSONObject || value instanceof Boolean || value instanceof String) {
             	// single value
             	addAttr(builder, key, value);
@@ -810,9 +816,6 @@ public class FreeIpaConnector extends AbstractRestConnector<FreeIpaConfiguration
         			|| attrName.equals(FreeIpaConnector.ATTR_MEMBEROF_GROUP)) {
         		continue; // proceeed in different way...
         	}
-        	
-
-        	
     		if (USER_MULTIVALUED.contains(attrName)) {
     			JSONArray values = new JSONArray();
     			if (attrValue!=null) {
@@ -837,6 +840,16 @@ public class FreeIpaConnector extends AbstractRestConnector<FreeIpaConfiguration
         
         JSONArray params_array = new JSONArray();
         params_array.put(create ? loginNew : uid.getUidValue());
+		//if we support preserved accounts, undelete before anything else
+		if ( getConfiguration().getSupportPreserved() ){
+			if (preserved_user_list.contains(loginNew)){
+				JSONObject undelrequest = getIpaRequest("user_undel" , new JSONObject(), params_array);
+				JSONObject undeljores = callRequest(undelrequest);
+				LOG.info("response after set user_undel UID: {0}, body: {1}", loginNew, undeljores);
+				preserved_user_list.remove(loginNew);
+			}
+		}
+
 		JSONObject request = getIpaRequest(create ? "user_add" : "user_mod", params, params_array);
         
         LOG.ok("user request (without password): {0}", request.toString());
@@ -846,37 +859,8 @@ public class FreeIpaConnector extends AbstractRestConnector<FreeIpaConfiguration
         }
 
         if (params.length()>0) {
-			// changed to support preserved accounts
-			JSONObject jores;
-			try {
-				jores = callRequest(request);
-				LOG.info("response UID: {0}, body: {1}", loginNew, jores);
-			} catch (AlreadyExistsException e) {
-				if ( getConfiguration().getSupportPreserved() ){
-					//try to undelete the account, if it is active an ConnectorIOException will be thrown
-					//verify that it is loginNew and " is already active"
-					// if so, re-throw already exists
-					try{
-						request = getIpaRequest("user_undel" , new JSONObject(), params_array);
-						jores = callRequest(request);
-						request = getIpaRequest("user_mod" , params, params_array);
-						jores = callRequest(request);
-					} catch (ConnectorIOException active_e){
-						//a bit ugly, but...
-						if (active_e.getMessage().contains(loginNew) && active_e.getMessage().contains("is already active")){
-							request = getIpaRequest("user_mod" , params, params_array);
-							jores = callRequest(request);
-							//throw new AlreadyExistsException(e);
-						}
-						else{
-							throw new ConnectorIOException(active_e);
-						}
-					}
-				}
-				else{ // if we don't use preserved accounts, rethrow the exception
-					throw new AlreadyExistsException(e);
-				}
-			}
+			JSONObject jores = callRequest(request);
+			LOG.info("response UID: {0}, body: {1}", loginNew, jores);
 		}
         
         handleEnable(attributes, loginNew, create);
